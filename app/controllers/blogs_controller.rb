072a940a095f45834fc2a6b94897dfd440d0fe89
ryota tanabe <teni0abe@gmail.com>
2019-03-26 12:17:48 +0900

    [add]createメソッドを作成、blog_paramsメソッドを作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,6 +1,6 @@
 class BlogsController < ApplicationController
   def index
-
+    @blogs = Blog.all
   end
 
   def new
@@ -8,6 +8,14 @@ class BlogsController < ApplicationController
   end
 
   def create
-    
+    Blog.create(blog_params)
+    redirect_to new_blog_path  #GETでBlogsコントローラのnewメソッドに飛ぶ(pathで相対パスを指定している)
+  end
+
+
+  private
+
+  def blog_params  #createやupdateでも使うのでDRYの観点からメソッドとして定義している
+    params.require(:blog).permit(:content)  #(content: params[:blog][:content])のStrong Parametersでの書き方。blogキーのtitleのみ取得を許可している)
   end
 end
