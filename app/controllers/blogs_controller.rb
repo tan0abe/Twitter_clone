6342aa6aaed9c82bc548e9c1fe33b840a05612e3
ryota tanabe <teni0abe@gmail.com>
2019-03-27 15:09:02 +0900

    [add]editアクションとedit.html.erbを作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -10,7 +10,7 @@ class BlogsController < ApplicationController
   def create
     @blog = Blog.new(blog_params)
     if @blog.save  #saveに失敗した場合は@blogの中にエラーの情報が詰め込まれているのでnew.html.erbでエラーメッセージを表示したりすることが出来ている
-    redirect_to blogs_path, noctice: "みんなについーとしました！"  #GETでBlogsコントローラのindexアクションに飛ぶ(pathで相対パスを指定している)
+    redirect_to blogs_path, notice: "みんなについーとしました！"  #GETでBlogsコントローラのindexアクションに飛ぶ(pathで相対パスを指定している)
     else
       render 'new'  #createアクションが実行されるとViewは自動的にcreate.html.erbを選択するが存在しないためエラーになるので、new.html.erbに変更している
     end
@@ -20,6 +20,8 @@ class BlogsController < ApplicationController
     @blog = Blog.find(params[:id])
   end
 
+  def edit
+  end
 
   private
 
