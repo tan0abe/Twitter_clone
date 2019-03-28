4a3e13144cd4a42d32fdf84c56937329757c0aef
ryota tanabe <teni0abe@gmail.com>
2019-03-28 18:10:56 +0900

    [update]routesにgetリクエストの/blogsを追加。(heroku)でアクセス出来るようにするため。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs do
+    get '/blogs' to: 'blogs#index'
     collection do  #idなどを必要としない固有のルーティングを作成(member doとするとidを必要とする固有のルーティングを作成する)
       post :confirm
     end
