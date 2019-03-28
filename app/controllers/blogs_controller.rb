5fedf9a2b480ba3b007f8674bf25568a90425e84
ryota tanabe <teni0abe@gmail.com>
2019-03-28 11:39:57 +0900

    [add]confirm.html.erbにもどるボタンを追加。blogs_controller.rbのnewアクションに戻った時に値が保持されるようにupdate

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,7 +6,11 @@ class BlogsController < ApplicationController
   end
 
   def new
+    if params[:back]
+      @blog = Blog.new(blog_params)
+    else
     @blog = Blog.new
+    end
   end
 
   def create
@@ -41,6 +45,7 @@ class BlogsController < ApplicationController
 
   def confirm
     @blog = Blog.new(blog_params)
+    render :new if@blog.invalid?
   end
 
   private
