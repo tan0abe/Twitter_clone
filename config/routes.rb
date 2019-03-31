fc8b43a26cfb3e285ac7ace4358456697575db5e
ryota tanabe <teni0abe@gmail.com>
2019-03-31 19:14:46 +0900

    [add]Twitterクローン完成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   get '/', to: 'blogs#index'
+  get 'boot', to: 'blogs#bootstrap'
+  get 'temp', to: 'blogs#template'
   resources :blogs do
     collection do  #idなどを必要としない固有のルーティングを作成(member doとするとidを必要とする固有のルーティングを作成する)
       post :confirm
